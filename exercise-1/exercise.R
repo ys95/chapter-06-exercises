08582f6d8f4b0361296da53fea1b7ea7dc121f8a
Joel Ross <joelross@uw.edu>
2018-06-01 09:46:30 -0700

    Style fixes

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -10,7 +10,7 @@ name_length <- nchar(my_name)
 # Print the number of letters in your name
 print(name_length)
 
-# Create a variable `now_doing` that is your name followed by "is programming!" 
+# Create a variable `now_doing` that is your name followed by "is programming!"
 # (use the `paste()` function)
 now_doing <- paste(my_name, "is programming!")
 
@@ -19,25 +19,25 @@ toupper(now_doing)
 
 ### Bonus
 
-# Pick two of your favorite numbers (between 1 and 100) and assign them to 
+# Pick two of your favorite numbers (between 1 and 100) and assign them to
 # variables `fav_1` and `fav_2`
 fav_1 <- 12
 fav_2 <- 87
 
 # Divide each number by the square root of 201 and save the new value in the
 # original variable
-fav_1 <- fav_1/sqrt(201)
-fav_2 <- fav_2/sqrt(201)
+fav_1 <- fav_1 / sqrt(201)
+fav_2 <- fav_2 / sqrt(201)
 
-# Create a variable `raw_sum` that is the sum of the two variables. Use the 
+# Create a variable `raw_sum` that is the sum of the two variables. Use the
 # `sum()` function for practice.
-raw_sum <-sum(fav_1, fav_2)
+raw_sum <- sum(fav_1, fav_2)
 
 # Create a variable `round_sum` that is the `raw_sum` rounded to 1 decimal place.
 # Use the `round()` function.
 round_sum <- round(raw_sum, 1)
 
-# Create two new variables `round_1` and `round_2` that are your `fav_1` and 
+# Create two new variables `round_1` and `round_2` that are your `fav_1` and
 # `fav_2` variables rounded to 1 decimal places
 round_1 <- round(fav_1, 1)
 round_2 <- round(fav_2, 1)
--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -1,12 +1,12 @@
 # Exercise 4: functions and conditionals
 
-# Define a function `is_twice_as_long` that takes in two character strings, and 
+# Define a function `is_twice_as_long` that takes in two character strings, and
 # returns whether or not (e.g., a boolean) the length of one argument is greater
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
 is_twice_as_long <- function(str1, str2) {
   diff <- abs(nchar(str1) - nchar(str2))
-  min_length <- min(nchar(str1), nchar(str2));
+  min_length <- min(nchar(str1), nchar(str2))
   diff >= min_length # if difference is more than short
 }
 
@@ -23,16 +23,16 @@ is_twice_as_long("dog", "cat")
 #   "Your first string is longer by N characters"
 #   "Your second string is longer by N characters"
 #   "Your strings are the same length!"
-describe_difference <- function(first,second) {
+describe_difference <- function(first, second) {
   diff <- nchar(first) - nchar(second)
-  if(diff > 0) {
+  if (diff > 0) {
     sentence <- paste("Your first string is longer by", diff, "characters")
-  } else if(diff < 0) {
+  } else if (diff < 0) {
     sentence <- paste("Your second string is longer by", -diff, "characters")
   } else {
     sentence <- "Your strings are the same length!"
   }
-  sentence  # return the sentence
+  sentence # return the sentence
 }
 
 # Call your `describe_difference` function by passing it different length strings
